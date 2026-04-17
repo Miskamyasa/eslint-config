372cf5e77c4c3cfa47c381672f607d5ea3e7a686
Denis Zaitsev <zaytsevdi@gmail.com>
2026-04-17 18:02:23 +0300

    feat: export createTypeScriptImportResolver from eslint-import-resolver-typescript

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "./configs/index"
 export * from "./factory"
 export * from "./globs"
 export * from "./types"
+export {createTypeScriptImportResolver} from "eslint-import-resolver-typescript"
 
 // eslint-disable-next-line import-x/no-default-export
 export {createConfig as default} from "./factory"
